71d88c4d23bb1b3652a8822532f7b1204ecb2249
Juan E Gonzalez <158267755+juan007-play@users.noreply.github.com>
2024-08-28 15:03:59 +0000

    JuanE-Simple-Counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Clock from "./clockimage";
+import Card from "./card";
 
 //create your first component
-const Home = () => {
+const SecondsCounter = ({seconds}) => {
+ 
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <>
+      <div className="container-fluid d-flex justify-content-center gap-2 bg-black mt-3">
+          <Clock/>
+          <Card number={Math.floor(seconds % 1000000 / 100000)}/>
+          <Card number={Math.floor(seconds % 100000 / 10000)}/>
+          <Card number={Math.floor(seconds % 10000 / 1000)}/>
+          <Card number={Math.floor(seconds % 1000 / 100)}/>
+          <Card number={Math.floor(seconds % 100 / 10)}/>
+          <Card number={seconds % 10}/>
+      </div>	
+  </>	
 	);
-};
+};  
 
-export default Home;
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,17 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import SecondsCounter from "./component/home.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
+const intervalID = setInterval(myCallback, 1000);
+
+let counter = 0
+function myCallback() {
+    // Your code here
+    counter++
+    root.render(<SecondsCounter seconds={counter}/>);
+ 
+}
